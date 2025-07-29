afde9989000ba442bc8c6d04025a2ae1623f9923
Win Steveker <win.steveker@gmx.de>
2025-07-30 01:36:07 +0200

    build modified

--- tests/test_video_vault/test_build/test_build.py
+++
@@ -1,9 +0,0 @@
-"""module."""
-
-import pytest
-
-
-@pytest.mark.skip(reason="skipping bc build is not easy to test")
-def test_do_build() -> None:
-    """Test func for build."""
-    raise NotImplementedError
---
+++ video_vault/build_project/build.py
@@ -0,0 +1,33 @@
+"""Build script for video_vault."""
+
+import tempfile
+from pathlib import Path
+
+from PyInstaller.__main__ import run
+
+from video_vault import build_project
+
+
+def do_build() -> None:
+    """Build the app."""
+    # Create temporary directory for build files
+    with tempfile.TemporaryDirectory() as temp_build_dir:
+        spec_file = Path(build_project.__file__).parent / "VideoVault.spec"
+
+        options = [
+            # Path to spec file
+            str(spec_file),
+            # Clean build files beforehand
+            "--clean",
+            # Set work and dist path to temp dir
+            "--workpath",
+            temp_build_dir,
+            # Set dist path to current dir
+            "--distpath",
+            ".",
+        ]
+        run(options)
+
+
+if __name__ == "__main__":
+    do_build()
