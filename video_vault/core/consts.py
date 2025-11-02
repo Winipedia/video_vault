5fbbbad07520e885299f916a7e24f05e54df5350
Winipedia <win.steveker@gmx.de>
2025-11-02 19:23:48 +0000

    artifacts build corectly

--- video_vault/core/consts.py
+++ video_vault/core/consts.py
@@ -1,10 +1,9 @@
 """This module contains constants for the application."""
 
-from winipedia_utils.projects.poetry.config import PyprojectConfigFile
 from winipedia_utils.text.string import make_name_from_obj
 
 import video_vault
 
 APP_NAME = make_name_from_obj(video_vault, split_on="_", join_on="", capitalize=True)
 
-AUTHOR = PyprojectConfigFile.get_main_author_name()
+AUTHOR = "Winipedia"
