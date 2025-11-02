1a73e570c50021b20fd67b76504c1a078975589b
Winipedia <win.steveker@gmx.de>
2025-11-02 08:12:45 +0000

    builds for realse added

---
+++ tests/test_video_vault/test_artifacts/test_build.py
@@ -0,0 +1,12 @@
+"""module."""
+
+import pytest
+
+
+class TestVideoVaultBuild:
+    """Test class for VideoVaultBuild."""
+
+    @pytest.mark.skip(reason="Not sure how to test this yet")
+    def test_get_artifacts(self) -> None:
+        """Test method for get_artifacts."""
+        raise NotImplementedError
--- video_vault/core/consts.py
+++ video_vault/core/consts.py
@@ -1,12 +1,10 @@
 """This module contains constants for the application."""
 
-from winipedia_utils.modules.package import make_name_from_package
 from winipedia_utils.projects.poetry.config import PyprojectConfigFile
+from winipedia_utils.text.string import make_name_from_obj
 
 import video_vault
 
-APP_NAME = make_name_from_package(
-    video_vault, split_on="_", join_on="", capitalize=True
-)
+APP_NAME = make_name_from_obj(video_vault, split_on="_", join_on="", capitalize=True)
 
 AUTHOR = PyprojectConfigFile.get_main_author_name()
