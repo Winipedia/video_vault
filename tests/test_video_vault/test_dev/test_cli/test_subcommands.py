1a42a8fd068222d26cfec2151a21b18f114b88ce
Winipedia <win.steveker@gmx.de>
2025-11-15 20:27:43 +0000

    trest fix

--- tests/test_video_vault/test_dev/test_cli/test_subcommands.py
+++ tests/test_video_vault/test_dev/test_cli/test_subcommands.py
@@ -1,14 +1,14 @@
 """module."""
 
-from pytest_mock import MockerFixture
-from winipedia_utils.utils.modules.module import make_obj_importpath
-
-from video_vault.dev.cli import subcommands
-from video_vault.dev.cli.subcommands import run
+from winipedia_utils.dev.projects.poetry.poetry import get_poetry_run_cli_cmd_args
+from winipedia_utils.utils.os.os import run_subprocess
+from winipedia_utils.utils.testing.assertions import assert_with_msg
 
 
-def test_run(mocker: MockerFixture) -> None:
+def test_run() -> None:
     """Test func for run."""
-    mock_main = mocker.patch(make_obj_importpath(subcommands) + ".main")
-    run()
-    mock_main.assert_called_once()
+    # call --help and verify it doesn't error
+    completed_process = run_subprocess([*get_poetry_run_cli_cmd_args(), "--help"])
+    assert_with_msg(completed_process.returncode == 0, "Should not error")
+    stdout = completed_process.stdout.decode("utf-8")
+    assert_with_msg("Run the app" in stdout, "Should show run command")
